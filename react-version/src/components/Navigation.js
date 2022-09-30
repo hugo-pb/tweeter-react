03e3a7c5f6d6215dc9e6ba06471c7669356756e1
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-30 17:00:34 -0600

    created components and cleaned up css files

---
+++ react-version/src/components/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navigation = () => {
+  return <h1>I am the navigation component</h1>;
+};
+
+export default Navigation;
---
+++ react-version/src/components/TweetList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TweetList = () => {
+  return <h1>I am the tweet list component</h1>;
+};
+
+export default TweetList;
